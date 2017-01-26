714dee55b0e07ff9000109fcdabda95b5f838f07
Joseph Sungpil Choi <chousemath@gmail.com>
2017-01-27 00:55:16 +0900

    trying to deploy a themeforest template to github pages 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,30 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: [
+    './app.component.css',
+    './css/animate.css',
+    './css/bootstrap.min.css',
+    './css/et-line-fonts.css',
+    './css/font-awesome.min.css',
+    './css/hamburgers.min.css',
+    './css/magnific-popup.css',
+    './css/meanmenu.css',
+    './css/owl.carousel.css',
+    './css/responsive.css',
+    './css/mobile_and_desktop.css',
+    './css/shortcode/animate-headline.css',
+    './css/shortcode/blog.css',
+    './css/shortcode/breadcrumb.css',
+    './css/shortcode/default.css',
+    './css/shortcode/footer.css',
+    './css/shortcode/hamburgers.min.css',
+    './css/shortcode/header.css',
+    './css/shortcode/portfolio.css',
+    './css/shortcode/service.css',
+    './css/shortcode/slider.css',
+    './css/shortcode/testimonial.css'
+  ]
 })
 export class AppComponent {
   title = 'app works!';
